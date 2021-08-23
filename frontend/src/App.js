f2fcefff2409e181e9d9090a6b3a5dfca256b379
ahmedseragcodes <ahmedserag1@gmail.com>
2021-08-23 11:22:25 -0400

    added icons for skill group

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,26 @@ function App() {
         <p>Ahmed Serag</p>
         <i class="fas fa-atom fa-xl"></i>
       </div>
+      <div className="skillsIconGroup">
+        <i class="fab fa-react fa-xl"></i>
+        <i class="fab fa-js-square fa-xl"></i>
+        <i class="fab fa-node-js fa-xl"></i>
+        <i class="fas fa-database fa-xl">SQLite</i>
+        <i class="fab fa-css3-alt fa-xl"></i>
+        <i class="fab fa-sass fa-xl"></i>
+        <i class="fab fa-html5 fa-xl"></i>
+        <i class="fab fa-github fa-xl"></i>
+        <i class="fab fa-git-square fa-xl"></i>
+
+
+
+      <i class="fab fa-google"></i>
+      <i class="fab fa-facebook"></i>
+
+
+
+
+      </div>
     </div>
   );
 }
