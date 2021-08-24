e0201cd182d423860d9afc37808931b8c381bf68
ahmedseragcodes <ahmedserag1@gmail.com>
2021-08-24 13:06:29 -0400

    updated portfolio image

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import PokemonImg from "./PortfolioPokemonPic.PNG";
 import FamilyPromiseImg from "./PortfolioFamilyPromisePic.png";
 import CarSalesImg from "./PortfolioCarSalesPic.PNG";
-import PortfolioAuthorImg from "./PortfolioSelfie2.jpg";
+import PortfolioAuthorImg from "./PortfolioSelfie4.jpg";
 import AFBackendImg from "./PortfolioAFBackend.jpg";
 import CalculatorImg from "./PortfolioCalculator.PNG";
 import SignInSheetImg from "./PortfolioSignInSheet.PNG";
